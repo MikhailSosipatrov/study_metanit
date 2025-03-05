1b12055b57da18e377638491e27a571ecd4a62a1
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-05 09:36:22 +0300

    Ch. 2 lesson 1

---
+++ JAVA_SE/src/chpater/second/Main.java
@@ -0,0 +1,14 @@
+package chpater.second;
+
+/*
+многострочный комментарий
+Объявление класса Main,
+который содержит код программы
+*/
+public class Main {
+
+    //Определение метода main
+    public static void main(String[] args) {
+        System.out.println("Hello java!"); //Вывод строки в консоль
+    }
+}
