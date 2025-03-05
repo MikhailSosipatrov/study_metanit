a444f5f0fe94fe1fdce96a5dfd8bcbabb22d5206
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-05 09:57:54 +0300

    Ch. 2 lesson 3

--- JAVA_SE/src/chpater/second/Main.java
+++ JAVA_SE/src/chpater/second/Main.java
@@ -9,22 +9,38 @@ public class Main {
 
     //Определение метода main
     public static void main(String[] args) {
-        System.out.println("Hello java!"); //Вывод строки в консоль
-        int x;
-        x = 10;
-        System.out.println(x);
+        boolean isActive = true;
+        System.out.println(isActive);
 
-        int y = 20;
-        System.out.println(y);
+        byte a = 3; //-128 до 127
+        System.out.println(a);
 
-        int z, v;
+        short b = 4;
+        System.out.println(b); //-32768 до 32767
 
-        int a = 10, b = 20;
+        int c = 4;
+        System.out.println(a); //-2147483648 до 2147483647
 
-        var f = 4.1;
+        long d = 5;
+        System.out.println(d);
+
+        double e = 6;
+        System.out.println(e);
+
+        float f = 8.5F;
         System.out.println(f);
 
-        final int LIMIT = 5;
-        System.out.println(LIMIT);
+        char g = 'a';
+        System.out.println(g);
+
+        char ch = 100;
+        System.out.println(ch);
+
+        String text = """
+                Лишь тем, кем бой за жизнь изведан,
+                жизнь и свободу заслужил.
+                """;
+
+        System.out.println(text);
     }
 }
