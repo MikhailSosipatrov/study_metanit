e7e60672d85c943244bfe1a308990f40450388d7
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-05 13:41:09 +0300

    Ch. 2 lesson 2

--- GOLANG/main.go
+++ GOLANG/main.go
@@ -8,4 +8,22 @@ import (
 func main() {
 	fmt.Println("Hello World")
 	//Однострочный комментарий
+	var hello string = "Hello World"
+	fmt.Println(hello)
+
+	var a, b, c string
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	var (
+		name string = "Tom"
+		age  int    = 27
+	)
+
+	fmt.Println(name)
+	fmt.Println(age)
+
+	short_name := "Mike"
+	fmt.Println(short_name)
 }
