c0f3a4cbfcc07c6f9ea18aa455f2fd3cf04ad3a3
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-05 09:41:52 +0300

    Ch. 2 lesson 2

--- JAVA_SE/src/chpater/second/Main.java
+++ JAVA_SE/src/chpater/second/Main.java
@@ -10,5 +10,21 @@ public class Main {
     //Определение метода main
     public static void main(String[] args) {
         System.out.println("Hello java!"); //Вывод строки в консоль
+        int x;
+        x = 10;
+        System.out.println(x);
+
+        int y = 20;
+        System.out.println(y);
+
+        int z, v;
+
+        int a = 10, b = 20;
+
+        var f = 4.1;
+        System.out.println(f);
+
+        final int LIMIT = 5;
+        System.out.println(LIMIT);
     }
 }
