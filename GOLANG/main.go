3153d615da44029fab575853111afc8cc591f32c
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-06 15:24:15 +0300

    GOLANG Ch. 2 lesson 4

--- GOLANG/main.go
+++ GOLANG/main.go
@@ -4,26 +4,21 @@ import "fmt"
 
 // Многострочный комментарий
 func main() {
-	var a int8 = -4
-	var b uint64 = 5
-	var c rune = 10
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
+	const pi float64 = 3.1415926
+	fmt.Println(pi)
 
-	var fl float64 = 3.14
-	fmt.Println(fl)
+	const (
+		a = 5
+		b = 65
+	)
 
-	var comp complex64 = 1 + 2i
-	fmt.Println(comp)
+	const c, d = 1, 2
 
-	var isActive bool = true
-	fmt.Println(isActive)
-
-	var name string = "Tom Soier"
-	fmt.Println(name)
-
-	first, secong := 1, "str"
-	fmt.Println(first, secong)
+	const (
+		q = 100
+		r
+		t
+		y
+	)
 
 }
