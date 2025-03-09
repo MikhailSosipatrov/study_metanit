bf81a4dc61f3333a4f210c16d1164f0257b129fd
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-09 19:42:39 +0300

    JAVA_SE Ch. 2 lesson 5

--- JAVA_SE/src/chpater/second/Main.java
+++ JAVA_SE/src/chpater/second/Main.java
@@ -1,5 +1,6 @@
 package chpater.second;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 /*
@@ -11,13 +12,15 @@ public class Main {
 
     //Определение метода main
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        System.out.print("Bye!");
-        System.out.printf("x=%s", "stroka");
-        Scanner in = new Scanner(System.in);
-        System.out.println("Input a number: ");
-        int x = in.nextInt();
-        System.out.println("Your number is: " + x);
-        in.close();
+        double d = 2.0 - 1.1;
+        System.out.println(d);
+        BigDecimal b = BigDecimal.valueOf(2.0 - 1.1);
+        System.out.println(b);
+
+        BigDecimal a = new BigDecimal("2.0");
+        System.out.println(a);
+        BigDecimal b1 = new BigDecimal("1.1");
+        System.out.println(b1);
+        System.out.println(a.subtract(b1));
     }
 }
