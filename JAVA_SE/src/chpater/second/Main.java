d2bcd0cc2bc3cd69fb664a3dfb0f9d5636d8170a
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-10 18:33:23 +0300

    JAVA_SE Ch. 2 lesson 6

--- JAVA_SE/src/chpater/second/Main.java
+++ JAVA_SE/src/chpater/second/Main.java
@@ -3,24 +3,11 @@ package chpater.second;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-/*
-многострочный комментарий
-Объявление класса Main,
-который содержит код программы
-*/
 public class Main {
-
-    //Определение метода main
     public static void main(String[] args) {
-        double d = 2.0 - 1.1;
-        System.out.println(d);
-        BigDecimal b = BigDecimal.valueOf(2.0 - 1.1);
-        System.out.println(b);
+        int a = 0100;
+        int b = 0100;
 
-        BigDecimal a = new BigDecimal("2.0");
-        System.out.println(a);
-        BigDecimal b1 = new BigDecimal("1.1");
-        System.out.println(b1);
-        System.out.println(a.subtract(b1));
+        System.out.println((a&b));
     }
 }
