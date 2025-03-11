5f59e1c1c8f308b46fd972af9d49beed71a6b581
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-11 16:18:36 +0300

    GOLANG Ch. 2 lesson 11

--- GOLANG/main.go
+++ GOLANG/main.go
@@ -4,29 +4,14 @@ import "fmt"
 
 // Многострочный комментарий
 func main() {
-	for i := 10; i > 0; i-- {
-		fmt.Println(i)
-	}
+	add(1, 2, 3)
+	add(1, 2, 3, 4)
+}
 
-	j := 0
-	for ; j < 10; j++ {
-		fmt.Println(j)
+func add(numbers ...int) {
+	var sum = 0
+	for _, number := range numbers {
+		sum += number
 	}
-
-	m := 0
-	for m < 10 {
-		fmt.Println(m)
-		m++
-	}
-
-	users := [10]int{}
-
-	for _, user := range users {
-		fmt.Println(user)
-	}
-
-	var u = [5]string{"Tom", "John", "Julia", "Katya", "Senya"}
-	for i := len(u) - 1; i >= 0; i-- {
-		fmt.Println(u[i])
-	}
+	fmt.Println(sum)
 }
