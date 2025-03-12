715be57e199876fe3cf03e3328e91447d076414a
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-12 17:21:28 +0300

    GOLANG Ch. 2 lesson 13

--- GOLANG/main.go
+++ GOLANG/main.go
@@ -2,14 +2,47 @@ package main
 
 import "fmt"
 
-// Многострочный комментарий
-func main() {
-	x, z := add(1, 4, "Tom", "Bobs")
-	fmt.Println(x, z)
+func isEven(n int) bool {
+	return n%2 == 0
 }
 
-func add(x, y int, firstName, lastName string) (int, string) {
-	var z int = x + y
-	var fullname = firstName + " " + lastName
-	return z, fullname
+func isPositive(n int) bool {
+	return n > 0
+}
+
+func sum(numbers []int, criteria func(int) bool) int {
+	result := 0
+	for _, n := range numbers {
+		if criteria(n) {
+			result += n
+		}
+	}
+	return result
+}
+
+func add(x int, y int) int      { return x + y }
+func subtract(x int, y int) int { return x - y }
+func multiply(x int, y int) int { return x * y }
+
+func selectFunction(n int) func(int, int) int {
+	if n == 1 {
+		return add
+	} else if n == 2 {
+		return subtract
+	} else {
+		return multiply
+	}
+}
+
+func main() {
+	slice := []int{-2, 4, 3, -1, 7, -4, 23}
+
+	sumOfEven := sum(slice, isEven)
+	fmt.Println(sumOfEven)
+
+	sumOfPositives := sum(slice, isPositive)
+	fmt.Println(sumOfPositives)
+
+	f := selectFunction(1)
+	fmt.Println(f(1, 2))
 }
