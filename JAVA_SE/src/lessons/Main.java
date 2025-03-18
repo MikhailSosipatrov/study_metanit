7441bc5f9ea79a1769185dba728d58528d96204c
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-03-18 13:22:28 +0300

    JAVA_SE Ch. 2 lesson 9

---
+++ JAVA_SE/src/lessons/Main.java
@@ -0,0 +1,10 @@
+package lessons;
+
+public class Main {
+    public static void main(String[] args) {
+        float b = 56.98765f;
+
+        int a = (int) b;
+        System.out.println(a);
+    }
+}
