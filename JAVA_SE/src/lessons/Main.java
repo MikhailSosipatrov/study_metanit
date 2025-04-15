1774dfcbf145825f485ac3fad69d2ba49905f946
Mikhail Sosipatrov <mikhailwork194@gmail.com>
2025-04-15 15:37:30 +0300

    JAVA_SE Ch. 2 lesson 13

--- JAVA_SE/src/lessons/Main.java
+++ JAVA_SE/src/lessons/Main.java
@@ -2,11 +2,15 @@ package lessons;
 
 public class Main {
     public static void main(String[] args) {
-       int nums[] = new int[4];
-       int[] nums2 = new int[5];
-       int[] nums3 = new int[] {1, 2, 3, 4, 5};
-       nums3[0] = 100;
-       System.out.println(nums3.length);
+        hello();
+        hello2();
+    }
 
+    static void hello(){
+        System.out.println("hello");
+    }
+
+    static void hello2(){
+        System.out.println("hello2");
     }
 }
